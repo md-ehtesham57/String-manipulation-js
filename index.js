b1d7e03021b203400331d03573b14c197e13d675
Md Ehtesham <mdehtesham313@gmail.com>
2025-04-10 10:29:23 +0530

    String manipulations

---
+++ index.js
@@ -0,0 +1,26 @@
+let str1 = "Ehtesham"
+
+//Template literals for long lines and variable embading!
+let str2 = `hello ${str1}!`
+
+// console.log(str2);
+
+//To find the length of the string!
+console.log(str2.length);
+
+//For type Chanking
+console.log(typeof(str2[5]));
+
+//To check Characaters at a specific index!
+console.log(typeof(str2.charAt(5)));
+
+//
+console.log(str2.slice(3, 7));
+
+console.log(str1.substring(2,4));
+
+console.log(str1.toUpperCase());
+
+console.log(str1.toLowerCase());
+
+console.log("Eh".trim());
